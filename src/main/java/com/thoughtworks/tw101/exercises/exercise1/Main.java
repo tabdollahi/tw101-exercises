6f8739b0d7018618440a502a23e3d5984595e097
tabdollahi <abdollahi.tania@gmail.com>
2015-12-21 14:38:55 -0800

    add printOddNums method

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -4,6 +4,17 @@ package com.thoughtworks.tw101.exercises.exercise1;
 
 public class Main {
     public static void main(String[] args) {
+        printOddNums(1,100);
+    }
 
+    private static void printOddNums (int start, int end){
+        int sum = 0;
+        for (int i = start; i < end; i++){
+            if (i%2!=0){
+                System.out.println(i);
+                sum += i;
+            }
+        }
+        System.out.println(sum);
     }
 }
