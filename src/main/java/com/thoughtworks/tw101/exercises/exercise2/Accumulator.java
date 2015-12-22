c0a5001932526434acfdb12fd260d833219ada97
tabdollahi <abdollahi.tania@gmail.com>
2015-12-21 17:51:16 -0800

    add increment and total methods

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,12 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+    private int incrementCount = 0;
+
+    public void increment(){
+        incrementCount+=1;
+    }
+    public void total(){
+        System.out.println(incrementCount);
+    }
 }
