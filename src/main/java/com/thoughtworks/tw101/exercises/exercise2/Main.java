838438ab723dfd986cc4485ce7c5210eb43025cf
tabdollahi <abdollahi.tania@gmail.com>
2015-12-21 17:45:21 -0800

    call increment and total methods in main

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Main.java
@@ -7,7 +7,11 @@ public class Main {
     public static void main(String[] args) {
 
         Accumulator accumulator = new Accumulator();
+        for (int i = 0; i < 5; i++) {
+            accumulator.increment();
+        }
 
-
+        accumulator.total();
+        
     }
 }
