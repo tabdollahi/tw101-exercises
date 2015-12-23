b5baba3887db207c0fc42051434f340a889f471c
tabdollahi <abdollahi.tania@gmail.com>
2015-12-22 16:05:04 -0800

    complete exercise, finish rectangle area method and average method

--- src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
@@ -3,6 +3,10 @@ package com.thoughtworks.tw101.exercises.exercise5;
 public class RectangleAverager {
 
     public float averageArea(Rectangle[] rectangles) {
-        return 0;
+        int sum = 0;
+        for (Rectangle r : rectangles){
+          sum += r.area();
+        }
+        return sum / rectangles.length;
     }
 }
