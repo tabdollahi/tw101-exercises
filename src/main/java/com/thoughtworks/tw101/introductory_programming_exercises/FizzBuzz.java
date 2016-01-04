54575ce39801e312a76caa5421bbe24624adf8fe
tabdollahi <abdollahi.tania@gmail.com>
2016-01-03 21:33:37 -0800

    solve FizzBuzz

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
@@ -32,6 +32,20 @@ public class FizzBuzz {
     }
 
     private static void fizzBuzz() {
+        for (int i = 1; i < 101; i++) {
+            if (i % 5 == 0 && i % 3 == 0) {
+                System.out.println("FizzBuzz");
+            }
+            else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
 
     }
 }
